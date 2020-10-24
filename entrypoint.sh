671d8e856ff9c8d94f2ba10561d1e66fcb8a42e3
JRozen <thequetran@gmail.com>
2020-10-24 14:42:00 +0200

    dix entryponit

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,24 @@ hLine() {
   echo "├─────────────────────────────────────────────────────────────────────"
 }
 
-manage="$BASE_DIR/manage.py"
+PORT=$PORT
+ALLOWED_HOSTS=$ALLOWED_HOSTS
+ETEBASE_DB_NAME=$ETEBASE_DB_NAME
+SUPER_USER=$SUPER_USER
+SUPER_EMAIL=$SUPER_EMAIL
+SUPER_PASS=$SUPER_PASS
 
+base_dir=/etebase
+manage="$base_dir/manage.py"
+
+# ADJUST INI CONFIG
 ALLOWED_HOSTS=${ALLOWED_HOSTS:-localhost}
-sed -i "s/%ALLOWED_HOSTS%/$ALLOWED_HOSTS/g" "$BASE_DIR/etebase-server.ini"
-cat "$BASE_DIR/etebase-server.ini"
+sed -i "s/%ALLOWED_HOSTS%/$ALLOWED_HOSTS/g" "$base_dir/etebase-server.ini"
+
+ETEBASE_DB_NAME=${ETEBASE_DB_NAME:-db.sqlite3}
+sed -i "s/%ETEBASE_DB_NAME%/$ETEBASE_DB_NAME/g" "$base_dir/etebase-server.ini"
+
+cat "$base_dir/etebase-server.ini"
 echo
 
 if "$manage" showmigrations -l | grep -q ' \[ \] 0001_initial'; then
@@ -33,27 +46,14 @@ if "$manage" showmigrations -l | grep -q ' \[ \] 0001_initial'; then
 
     export DJANGO_SUPERUSER_PASSWORD=$SUPER_PASS
     "$manage" createsuperuser --username "$SUPER_USER" --email "$SUPER_EMAIL" --noinput
-
     #    "$manage" shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$SUPER_USER' , '$SUPER_EMAIL', '$SUPER_PASS')"
   fi
-fi
-
-if [ -z "$USE_POSTGRES" ]; then
-  chown -R "$PUID:$PGID" "$DATA_DIR"
 fi
 
 hLine
 "$manage" showmigrations --list | grep -v '\[X\]'
 "$manage" makemigrations
 "$manage" migrate
-
-if [ ! -e "$STATIC_DIR/static/admin" ] || [ ! -e "$STATIC_DIR/static/rest_framework" ]; then
-  echo 'Static files are missing, running manage.py collectstatic...'
-  mkdir -p "$STATIC_DIR/static"
-  "$manage" collectstatic
-  chown -R "$PUID:$PGID" "$STATIC_DIR"
-  chmod -R a=rX "$STATIC_DIR"
-fi
 
 hLine
 echo 'Starting ETEBASE server'
