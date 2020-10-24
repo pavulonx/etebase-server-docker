5fa4e7c99c5a511ad949ab8a76f5f76ea0b91e51
JRozen <thequetran@gmail.com>
2020-10-24 13:45:42 +0200

    fix entrypoint create user

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,25 +12,30 @@ hLine() {
 
 manage="$BASE_DIR/manage.py"
 
-ALLOWED_HOSTS=${ALLOWED_HOSTS:-"$(hostname)"}
+ALLOWED_HOSTS=${ALLOWED_HOSTS:-localhost}
 sed -i "s/%ALLOWED_HOSTS%/$ALLOWED_HOSTS/g" "$BASE_DIR/etebase-server.ini"
 cat "$BASE_DIR/etebase-server.ini"
+echo
 
 
-if "$manage" showmigrations -l journal | grep -q ' \[ \] 0001_initial'; then
+if "$manage" showmigrations -l | grep -q ' \[ \] 0001_initial'; then
   hLine  
   echo 'Create Database'
   "$manage" migrate
   
-  if [ -n "$SUPER_USER" ]; then
+  if [ -n "$SUPER_USER" ] && [ -n "$SUPER_EMAIL" ]; then
     hLine
     if [ -z "$SUPER_PASS" ]; then
       SUPER_PASS=$(openssl rand -base64 31)
-      echo "Admin Password: $SUPER_PASS"
+      echo "Admin $SUPER_USER password: $SUPER_PASS"
     fi
 
     echo 'Creating Super User'
-    echo "from django.contrib.auth.models import User; User.objects.create_superuser('$SUPER_USER' , '$SUPER_EMAIL', '$SUPER_PASS')" | python manage.py shell
+
+    export DJANGO_SUPERUSER_PASSWORD=$SUPER_PASS
+    "$manage" createsuperuser --username "$SUPER_USER" --email "$SUPER_EMAIL" --noinput
+
+#    "$manage" shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$SUPER_USER' , '$SUPER_EMAIL', '$SUPER_PASS')"
   fi
 fi
 
@@ -40,6 +45,7 @@ fi
 
 hLine
 "$manage" showmigrations --list | grep -v '\[X\]'
+"$manage" makemigrations
 "$manage" migrate
 
 if [ ! -e "$STATIC_DIR/static/admin" ] || [ ! -e "$STATIC_DIR/static/rest_framework" ]; then
